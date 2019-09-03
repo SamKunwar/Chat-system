53fb826f72e096a1285b5d4e6380501f31014eee
Samiben1 <samiben498@gmail.com>
2019-09-03 15:20:58 +1000

    finally error free

--- chat/src/app/app.component.ts
+++ chat/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { AuthenticationService } from './services';
+import { User, Role } from './models';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-  title = 'chat';
+    currentUser: User;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    get isSuperAdmin() {
+        return this.currentUser && (this.currentUser.role === Role.SuperAdmin );
+    }
+
+    get isAdmin() {
+        return this.currentUser && (this.currentUser.role === Role.SuperAdmin || this.currentUser.role === Role.GroupAdmin);
+    }
+
+    get isAdminOrAssis() {
+        return this.currentUser && (this.currentUser.role === Role.SuperAdmin || this.currentUser.role === Role.GroupAdmin || this.currentUser.role === Role.GroupAssis);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
 }
--- chat/src/app/socket.service.ts
+++ chat/src/app/socket.service.ts
@@ -6,7 +6,7 @@ import * as io from 'socket.io-client';
   providedIn: 'root'
 })
 export class SocketService {
-
+/*
 	private url = 'http://localhost:3000';
 	private socket = io(this.url);
 	observable = new Observable(observer=>
@@ -22,5 +22,54 @@ export class SocketService {
 	}
 
 	}
+*/
+private socket = io('http://localhost:3000');
 
+    joinRoom(data)
+    {
+        this.socket.emit('join',data);
+    }
 
+    newUserJoined()
+    {
+        let observable = new Observable<{user:String, message:String}>(observer=>{
+            this.socket.on('new user joined', (data)=>{
+                observer.next(data);
+            });
+            return () => {this.socket.disconnect();}
+        });
+
+        return observable;
+    }
+
+    leaveRoom(data){
+        this.socket.emit('leave',data);
+    }
+
+    userLeftRoom(){
+        let observable = new Observable<{user:String, message:String}>(observer=>{
+            this.socket.on('left room', (data)=>{
+                observer.next(data);
+            });
+            return () => {this.socket.disconnect();}
+        });
+
+        return observable;
+    }
+
+    sendMessage(data)
+    {
+        this.socket.emit('message',data);
+    }
+
+    newMessageReceived(){
+        let observable = new Observable<{user:String, message:String}>(observer=>{
+            this.socket.on('new message', (data)=>{
+                observer.next(data);
+            });
+            return () => {this.socket.disconnect();}
+        });
+
+        return observable;
+    }
+}
